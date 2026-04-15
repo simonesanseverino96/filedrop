5d05ae12ba90b6778495aefcd646429a83ccf911
simone sanseverino <simonesanseverino45@gmail.com>
2026-04-15 14:58:57 +0200

    Fix Stripe API version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-04-10',
+apiVersion: '2023-10-16',
 })
 
 export const getStripeCustomerPortalUrl = async (customerId: string): Promise<string> => {
