c758ff66ef9595aedbdb51ffb07e41ade0c5d99a
simone sanseverino <simonesanseverino45@gmail.com>
2026-04-15 14:55:16 +0200

    Fix Stripe type

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -19,7 +19,7 @@ export async function POST(req: NextRequest) {
 
   switch (event.type) {
     case 'checkout.session.completed': {
-      const session = event.data.object as Stripe.CheckoutSession
+      const session = event.data.object as Stripe.Checkout.Session
       const userId = session.metadata?.supabase_user_id
       const plan = session.metadata?.plan
       if (userId && plan) {
